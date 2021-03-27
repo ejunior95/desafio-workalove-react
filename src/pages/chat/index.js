db8acec993d7635346d1eab8eb8e392947d7693b
ejunior95 <e.junior95@gmail.com>
2021-03-27 18:03:40 -0300

    [UPDATE]-> Preparação do ambiente e estruturação

---
+++ src/pages/chat/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import  Container  from './styles';
+
+function Home() {
+  return(
+    <Container>
+        <h1>Chat</h1>
+    </Container>
+  );
+}
+
+export default Home;
